c333da34157fae556676112a1273a495271e42f2
Mack McCall <mackmc9924@gmail.com>
2026-07-07 23:21:56 -0400

    created teacher entity, repository, and service

---
+++ backend-api/src/main/java/com/example/ScheduleSidekick/entity/Teacher.java
@@ -0,0 +1,47 @@
+package com.example.ScheduleSidekick.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "teachers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long teacherId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String department;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String bio;
+
+    public Teacher(String name, String email, String password, String department, String bio) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.department = department;
+        this.bio = bio;
+    }
+
+}
